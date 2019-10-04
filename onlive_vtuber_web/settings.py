aad63ab8068da322d548b702cca3759a15ad7a53
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-04 18:27:38 +0900

    Update settings.py

--- onlive_vtuber_web/settings.py
+++ onlive_vtuber_web/settings.py
@@ -124,6 +124,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
