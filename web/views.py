df61c024b5a7fc1174ef81a486eff2ce214754ef
狐さん <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-04 18:20:44 +0900

    Update views.py
    delete testdata

--- web/views.py
+++ web/views.py
@@ -17,25 +17,6 @@ def index(request):
                          'gender': i['uid']['gender'],
                          'production': i['uid']['production']})
 
-    #test
-    livedata.append({'liver_name': 'test name pr=にじさんじ',
-                     'live_url': 'Sun-ggImuVg',
-                     'live_title': 'test title',
-                     'start_time': '00:00:00',
-                     'gender': 'woman',
-                     'production': 'にじさんじ'})
-
-    livedata.append({'liver_name': 'test name pr=ホロライブ',
-                     'live_url': 'Sun-ggImuVg',
-                     'live_title': 'test title',
-                     'start_time': '00:00:00',
-                     'gender': 'man',
-                     'production': 'ホロライブ'})
-
-
-
-    #data
-
     data['livedata'] = livedata
     print(data)
     return render(request, 'index.html', data)
