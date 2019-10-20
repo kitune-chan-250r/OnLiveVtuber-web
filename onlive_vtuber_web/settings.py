82f87dd0608ff6ad8e7aeb4089f78d1884e9dc5e
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-21 03:10:26 +0900

    hontowakaran

--- onlive_vtuber_web/settings.py
+++ onlive_vtuber_web/settings.py
@@ -151,7 +151,7 @@ PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
 PWA_APP_ICONS = [
     {
-        'src': '/static/images/my_app_icon.png',
+        'src': '/static/images/icon-144x144.png',
         'sizes': '160x160'
     }
 ]
