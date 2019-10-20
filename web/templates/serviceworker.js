ee8a04898e98836df361ef11c81572b58a809558
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-21 02:59:10 +0900

    Update serviceworker.js

--- web/templates/serviceworker.js
+++ web/templates/serviceworker.js
@@ -2,6 +2,30 @@
 
 var staticCacheName = 'djangopwa-v1';
 
+var filesToCache = [
+    '/',
+    //'/offline',
+    '/css/django-pwa-app.css',
+    '/images/icons/icon-72x72.png',
+    '/images/icons/icon-96x96.png',
+    '/images/icons/icon-128x128.png',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-152x152.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-384x384.png',
+    '/images/icons/icon-512x512.png',
+    '/static/images/icons/splash-640x1136.png',
+    '/static/images/icons/splash-750x1334.png',
+    '/static/images/icons/splash-1242x2208.png',
+    '/static/images/icons/splash-1125x2436.png',
+    '/static/images/icons/splash-828x1792.png',
+    '/static/images/icons/splash-1242x2688.png',
+    '/static/images/icons/splash-1536x2048.png',
+    '/static/images/icons/splash-1668x2224.png',
+    '/static/images/icons/splash-1668x2388.png',
+    '/static/images/icons/splash-2048x2732.png'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
