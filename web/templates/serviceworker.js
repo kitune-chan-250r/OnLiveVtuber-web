444140f6f9a4fd3dc8e5379afe06519592898c1b
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-21 02:15:46 +0900

    Update serviceworker.js

--- web/templates/serviceworker.js
+++ web/templates/serviceworker.js
@@ -1,41 +1,28 @@
 // Base Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in settings.py
 
-var staticCacheName = "django-pwa-v" + new Date().getTime();
+var staticCacheName = 'djangopwa-v1';
 
-// Cache on install
-self.addEventListener("install", event => {
-    this.skipWaiting();
-    event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll(filesToCache);
-            })
-    )
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/base_layout'
+      ]);
+    })
+  );
 });
 
-// Clear cache on activate
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cacheName => (cacheName.startsWith("django-pwa-")))
-                    .filter(cacheName => (cacheName !== staticCacheName))
-                    .map(cacheName => caches.delete(cacheName))
-            );
-        })
-    );
-});
-
-// Serve from Cache
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+      if ((requestUrl.pathname === '/')) {
+        event.respondWith(caches.match('/base_layout'));
+        return;
+      }
+    }
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match('offline');
-            })
-    )
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
 });
