e8e462adea3eaaea5ec73a145f868abc2b6dfc4d
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-21 01:05:47 +0900

    save

--- onlive_vtuber_web/urls.py
+++ onlive_vtuber_web/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main', include('web.urls')),
-    #path('', include('pwa.urls')), #pwa
+    path('', include('pwa.urls')), #pwa
 ]
--- onlive_vtuber_web/wsgi.py
+++ onlive_vtuber_web/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-#from dj_static import Cling
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlive_vtuber_web.settings')
 
-#application = Cling(get_wsgi_application()) #heroku
-application = get_wsgi_application() #local
+application = Cling(get_wsgi_application()) #heroku
+#application = get_wsgi_application() #local
 
