8a75247f6bc31433952139d3d75c49a9cab36bd3
kitune-chan-250r <nekoo.hisui@gmail.com>
2019-10-22 15:04:23 +0900

    Update sw.js

--- web/templates/sw.js
+++ web/templates/sw.js
@@ -1,24 +1,7 @@
-var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
-
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ...');
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_VERSION)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function() {
-            });
-        }
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
