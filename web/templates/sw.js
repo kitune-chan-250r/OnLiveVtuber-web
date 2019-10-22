b9575bcc18a0dcf0c2c6f6b28a73e9aff97463a6
kitune-chan-250r <nekoo.hisui@gmail.com>
2019-10-22 13:03:30 +0900

    fix

--- web/templates/sw.js
+++ web/templates/sw.js
@@ -1,2 +1,24 @@
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+
 self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ...');
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+            });
+        }
+      })
+  );
 });
