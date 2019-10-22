ab6cbb8ca0a4c058944757b4c8b71bba1b7e776f
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2019-10-22 20:58:44 +0900

    collectstatic and debug false

--- onlive_vtuber_web/settings.py
+++ onlive_vtuber_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '84u04z!#7scgw)c36*2+y0$!dqn4plowsg18k!7l2go4+6m5+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 
 
@@ -122,8 +122,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#ALLOWED_HOSTS = ["*"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -137,5 +137,5 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#GOOGLE_ANALYTICS_TRACKING_ID = os.environ["GOOGLE_ANALYTICS_TRACKING_ID"]
+GOOGLE_ANALYTICS_TRACKING_ID = os.environ["GOOGLE_ANALYTICS_TRACKING_ID"]
 
