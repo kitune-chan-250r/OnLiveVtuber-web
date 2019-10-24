ed08a96830950f4bbe06fce750d1b4f496572070
kitune-chan-250r <nekoo.hisui@gmail.com>
2019-10-24 15:01:21 +0900

    past time badge

--- web/views.py
+++ web/views.py
@@ -1,19 +1,48 @@
 from django.shortcuts import render
-import re
+import re, time
 from web import Vtuber_LiveStatus_API_lib as vlsa
+from datetime import timedelta
+from datetime import datetime
+from dateutil.parser import parse
+from pytz import timezone
+
+
 # Create your views here.
 BASE_URL = 'https://vtuber-livestatus-api.herokuapp.com/api/'
+
+#str型 timeを受け取り'%Y-%m-%d %H:%M:%S'のdatetime.timedeltを返す
+def time_jst(time):
+    p = parse(time).astimezone(timezone('Asia/Tokyo'))
+    return parse(p.strftime('%Y-%m-%d %H:%M:%S'))
+
+#str型　startを受け取りdatetime.timedeltのpastを返す
+def past_time(start):
+    now = datetime.now()
+    past = time_jst(str(now)) -  time_jst(start)
+    return past
+
+def deformed(start):
+    secs = past_time(start).total_seconds()
+    hours = int(secs / 3600)
+    minutes = int(secs / 60) % 60
+
+    if hours > 0:
+        return '約{}時間'.format(hours)
+    else:
+        return '約{}分'.format(minutes)
 
 def index(request):
     json_data = vlsa.get(BASE_URL + 'onlive/')
     
     data = {}
     livedata = []
+
     for i in json_data:
+        past = deformed(i['start_time'])
         livedata.append({'liver_name': i['uid']['liver_name'],
                          'live_url': i['live_url'].replace('https://www.youtube.com/watch?v=', ''),
                          'live_title': i['live_title'],
-                         'start_time': i['start_time'],
+                         'past_time': past,
                          'gender': i['uid']['gender'],
                          'production': i['uid']['production']})
 
