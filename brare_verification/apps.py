b510cdc003df26b9a64ee703052c6f740bb4760b
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2021-03-31 23:57:47 +0900

    fix

--- brare_verification/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrareVerificationConfig(AppConfig):
-    name = 'brare_verification'
---
+++ brave_verification/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BraveVerificationConfig(AppConfig):
+    name = 'brave_verification'
