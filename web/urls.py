10621ba3e2d04626e828ad9c4fe9072f54dabee7
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2021-03-31 22:15:43 +0900

    brave rewards

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import *
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', index, name='index'),
     path('reminder', reminder, name='reminder'),
     path('vtuber', vtuber, name='vtuber'),
-    path('about_this_page', about_this_page, name='about_this_page')
+    path('about_this_page', about_this_page, name='about_this_page'),
+    path('.well-known', TemplateView.as_view(template_name='brave-rewards-verification.txt', content_type='text/plain'))
 ]
