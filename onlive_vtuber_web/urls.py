ed1d51c9d10a25c885664b02d729fe6ec476c543
kitune-chan-250r <41228665+kitune-chan-250r@users.noreply.github.com>
2021-03-31 22:20:45 +0900

    fix

--- onlive_vtuber_web/urls.py
+++ onlive_vtuber_web/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     #pwa
     path('sw.js', (TemplateView.as_view(template_name="sw.js", 
   content_type='application/javascript', )), name='sw.js'),
+    #brave rewards
+    path('.well-known', TemplateView.as_view(template_name='brave-rewards-verification.txt',
+     content_type='text/plain')),
 ]
